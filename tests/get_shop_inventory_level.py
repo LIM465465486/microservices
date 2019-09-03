33664b7da18939913f29043baa23a79e206002c0
Luis <luisisaacmolina@outlook.com>
2019-09-03 00:14:10 -0700

    save

--- tests/get_shop_inventory_level.py
+++ tests/get_shop_inventory_level.py
@@ -1,3 +1,8 @@
+import requests
+import json
+from decimal import Decimal
+from config import *
+from auth.sap_auth import SAPAuth
 import shopify
 
 
@@ -14,21 +19,27 @@ def get_products():
     shopify.ShopifyResource.set_password(PASSWORD)
     shopify.ShopifyResource.set_site(shop_url)
 
+    shop_products = []
+    sap_products = []
 
     # Get the current shop
     products = get_all_resources(shopify.Product)
 
     for product in products:
-        product_attributes = {}
         product_variants = []
-        product_attributes = product.attributes
-        product_variants = product_attributes['variants']
-        for item in product_variants:
+        product_variants = product.attributes['variants']
+        for product_variant in product_variants:
             variant_attributes = {}
-            variant_attributes = item.attributes
-            print(variant_attributes['sku'])
+            variant_attributes = product_variant.attributes
+            # print(variant_attributes['sku'])
+            shop_products.append(dict(
+                product_id=product.id,
+                variant_id=product_variant.id,
+                sku=variant_attributes['sku'],
+                inventory_item_id=variant_attributes['inventory_item_id']
+            ))
 
-        print('A')
+    sap_products_list = get_sap_products_list()
 
 
 def get_all_resources(resource, **kwargs):
@@ -39,6 +50,25 @@ def get_all_resources(resource, **kwargs):
             kwargs.update({"limit": 250, "page": page})
             resources.extend(resource.find(**kwargs))
     return resources
+
+
+def get_sap_products_list():
+    print('Getting SAP products')
+    url = sap_url + '/v1/items'
+    token = SAPAuth.get_token()
+    headers = {'authorization': 'JWT ' + token, 'content-type': 'application/json'}
+    data = {"columns": ["ItemCode"],
+            "params": {}}
+    data = json.dumps(data)
+    # print(data)
+    r = requests.get(url, data=data, headers=headers)
+    # print(r.text)
+
+    sap_products_list = []
+    sap_products_list = json.loads(r.text)
+    # print(type(sap_products_list))
+    # print(sap_products_list)
+    return sap_products_list
 
 
 if __name__ == '__main__':
