0e3c467cacf5cd6514930af7beab6843fdea4bea
Luis <luisisaacmolina@outlook.com>
2019-10-09 21:48:00 -0700

    change shopify check from last id to update datetime

--- tests/import_sales_orders_shopify_to_sap.py
+++ tests/import_sales_orders_shopify_to_sap.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from datetime import date, datetime, timedelta
+from pytz import timezone
 from decimal import Decimal
 from config import *
 from auth.sap_auth import SAPAuth
@@ -17,99 +19,109 @@ def import_sales_orders():
 
     shop_orders = []
     sap_orders = []
-    since_id = get_sap_since_id()
+    # since_id = get_sap_since_id()
+
+    lookup_datetime = timezone('US/Pacific').localize(datetime.now() - timedelta(days=5)).replace(microsecond=0)
+    lookup_datetime = lookup_datetime.isoformat()
+
     # Get the current shop
-    orders = get_all_resources(shopify.Order, since_id=since_id, status='any')
+    # orders = get_all_resources(shopify.Order, since_id=since_id, status='any')
     # orders = get_all_resources(shopify.Order, ids=1419232018535, status='any')
+    orders = get_all_resources(shopify.Order, updated_at_min=lookup_datetime, status='any')
+    sap_lookup_date = date.today() - timedelta(days=15)
+    sap_lookup_date = sap_lookup_date.strftime('%Y-%m-%d')
+    shopify_order_ids = get_sap_orders_sync(sap_lookup_date)
 
-    for order in orders:
-        order_attributes = {}
-        order_attributes = order.attributes
+    if orders and shopify_order_ids:
 
-        if order_attributes['closed_at'] and order_attributes['fulfillment_status'] == 'fulfilled':
-            print('New order found, order id: ' + str(order_attributes['id']))
-            order_bill_address = {}
-            order_ship_address = {}
-            order_items_list = []
+        for order in orders:
+            order_attributes = {}
+            order_attributes = order.attributes
 
-            tax_code = '0'
+            if order_attributes['closed_at'] and order_attributes['fulfillment_status'] == 'fulfilled' and order_attributes['id'] not in shopify_order_ids:
+                print('New order found, order id: ' + str(order_attributes['id']))
+                order_bill_address = {}
+                order_ship_address = {}
+                order_items_list = []
 
-            sap_order = {}
-            sap_order['Lines'] = []
-            sap_order['Expenses'] = []
+                tax_code = '0'
 
-            sap_order['CardCode'] = 'T481995'
-            # sap_order['CardName'] = order['member_name']
-            sap_order['NumAtCard'] = str(order_attributes['order_number'])
-            sap_order['DocDate'] = order_attributes['created_at'][:10]
-            sap_order['DocDueDate'] = order_attributes['created_at'][:10]
-            sap_order['TaxDate'] = order_attributes['created_at'][:10]
-            sap_order['PickRemark'] = str(order_attributes['id'])
-            sap_order['DocTotal'] = float(order_attributes['total_price'])
+                sap_order = {}
+                sap_order['Lines'] = []
+                sap_order['Expenses'] = []
 
-            # sap_order[''] = order['coupon_discount']
-            # sap_order[''] = order['product_total']
-            # sap_order[''] = order['shipping_charge']
-            # print(order['tax'])
-            # sap_order['VatPercent'] = 7.00
-            # sap_order['DocTotal'] = order['grand_total']
-            sap_order['Comments'] = order_attributes['note'] if order_attributes['note'] else ''
-            # sap_order[''] = order['tracking_number']
+                sap_order['CardCode'] = 'T481995'
+                # sap_order['CardName'] = order['member_name']
+                sap_order['NumAtCard'] = str(order_attributes['order_number'])
+                sap_order['DocDate'] = order_attributes['created_at'][:10]
+                sap_order['DocDueDate'] = order_attributes['created_at'][:10]
+                sap_order['TaxDate'] = order_attributes['created_at'][:10]
+                sap_order['PickRemark'] = str(order_attributes['id'])
+                sap_order['DocTotal'] = float(order_attributes['total_price'])
 
-            # order_bill_address = order['bill_address']
-            # order_ship_address = order['ship_address']
+                # sap_order[''] = order['coupon_discount']
+                # sap_order[''] = order['product_total']
+                # sap_order[''] = order['shipping_charge']
+                # print(order['tax'])
+                # sap_order['VatPercent'] = 7.00
+                # sap_order['DocTotal'] = order['grand_total']
+                sap_order['Comments'] = order_attributes['note'] if order_attributes['note'] else ''
+                # sap_order[''] = order['tracking_number']
 
-            list_index = 0
-            line_items = order_attributes['line_items']
-            for line_item in line_items:
-                line_item_attributes = line_item.attributes
-                sap_order['Lines'].insert(list_index, {
-                    'ItemCode': line_item_attributes['sku'],
-                    'Quantity': line_item_attributes['quantity'],
-                    'Price': line_item_attributes['price'],
-                    'TaxLiable': 0,
-                    'TaxCode': tax_code,
-                    # 'TaxPercentagePerRow': tax_percent,
-                    'DiscountPercent': 0
-                })
-                list_index = list_index + 1
-                print('Item Loop')
+                # order_bill_address = order['bill_address']
+                # order_ship_address = order['ship_address']
 
-            if order_attributes['total_tax'] != '0.00':
-                sap_order['Lines'].insert(list_index, {
-                    'ItemCode': 'Tax',
-                    'Quantity': 1,
-                    'Price': order_attributes['total_tax'],
-                    'TaxCode': tax_code,
-                    'DiscountPercent': 0
-                })
-                list_index = list_index + 1
-                print('Tax Loop')
-
-            list_index = 0
-            shipping_lines = order_attributes['shipping_lines']
-            for shipping_line in shipping_lines:
-                shipping_line_attributes = shipping_line.attributes
-                if shipping_line_attributes['price'] != '0.00':
-                    sap_order['Expenses'].insert(list_index, {
-                        'ExpenseCode': 3,
-                        'LineTotal': shipping_line_attributes['price'],
-                        'TaxCode': tax_code
+                list_index = 0
+                line_items = order_attributes['line_items']
+                for line_item in line_items:
+                    line_item_attributes = line_item.attributes
+                    sap_order['Lines'].insert(list_index, {
+                        'ItemCode': line_item_attributes['sku'],
+                        'Quantity': line_item_attributes['quantity'],
+                        'Price': line_item_attributes['price'],
+                        'TaxLiable': 0,
+                        'TaxCode': tax_code,
+                        # 'TaxPercentagePerRow': tax_percent,
+                        'DiscountPercent': 0
                     })
                     list_index = list_index + 1
-                    print('Shipping Loop')
+                    print('Item Loop')
 
-            print(sap_order)
-            sap_json = json.dumps(sap_order)
-            url = sap_url + '/v1/orders'
-            token = SAPAuth.get_token()
-            if token:
-                headers = {'authorization': 'JWT ' + token, 'content-type': 'application/json'}
-                r = requests.post(url, sap_json, headers=headers)
-                # print(r.status_code)
-                # print(r.text)
-        # print('Order Loop')
-    print('A')
+                if order_attributes['total_tax'] != '0.00':
+                    sap_order['Lines'].insert(list_index, {
+                        'ItemCode': 'Tax',
+                        'Quantity': 1,
+                        'Price': order_attributes['total_tax'],
+                        'TaxCode': tax_code,
+                        'DiscountPercent': 0
+                    })
+                    list_index = list_index + 1
+                    print('Tax Loop')
+
+                list_index = 0
+                shipping_lines = order_attributes['shipping_lines']
+                for shipping_line in shipping_lines:
+                    shipping_line_attributes = shipping_line.attributes
+                    if shipping_line_attributes['price'] != '0.00':
+                        sap_order['Expenses'].insert(list_index, {
+                            'ExpenseCode': 3,
+                            'LineTotal': shipping_line_attributes['price'],
+                            'TaxCode': tax_code
+                        })
+                        list_index = list_index + 1
+                        print('Shipping Loop')
+
+                print(sap_order)
+                sap_json = json.dumps(sap_order)
+                url = sap_url + '/v1/orders'
+                token = SAPAuth.get_token()
+                if token:
+                    headers = {'authorization': 'JWT ' + token, 'content-type': 'application/json'}
+                    r = requests.post(url, sap_json, headers=headers)
+                    print(r.status_code)
+                    print('Line Loop')
+            # print('Order Loop')
+        print('A')
 
 
 def get_all_resources(resource, **kwargs):
@@ -148,6 +160,31 @@ def get_sap_since_id():
 
             return since_id
 
+
+def get_sap_orders_sync(lookup_date):
+    print('Getting Shopify IDs from SAP')
+    url = sap_url + '/v1/orders'
+    token = SAPAuth.get_token()
+    if token:
+        headers = {'authorization': 'JWT ' + token, 'content-type': 'application/json'}
+        data = {"columns": ["PickRmrk"],
+                "params": {"CardCode": {"op": "=", "value": "T481995"}, "DocDate": {"op": ">=", "value": lookup_date}},
+                "LineColumns": ["LineNum", "ItemCode", "Price", "Quantity"]}
+        data = json.dumps(data)
+        # print(data)
+        r = requests.get(url, data=data, headers=headers)
+
+        if r.status_code == 200:
+            # print(r.text)
+
+            sap_orders = []
+            shopify_order_ids = []
+            sap_orders = r.json()
+            for sap_order in sap_orders:
+                if sap_order['PickRmrk']:
+                    shopify_order_ids.append(int(sap_order['PickRmrk']))
+
+            return shopify_order_ids
 
 if __name__ == '__main__':
     import_sales_orders()
